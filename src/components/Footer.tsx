59930dea5a69c7359b1c452062a23ce67bd9b652
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-12-07 13:09:07 +0100

    fix target _blank

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className="footer">
-      Made with love ❤️ and coffe ☕️ by <a href="https://github.com/ecali" target="_blank" rel="GitHub">Elvio Moretti</a> 
+      Made with love ❤️ and coffe ☕️ by <a href="https://github.com/ecali" target="_blank" rel="noreferrer">Elvio Moretti</a> 
     </div>
   );
 };
