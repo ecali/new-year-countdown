2c8f675992ae03bcbabeb85a7a44d55fe7fd37f0
Elvio Moretti <78408765+ecali@users.noreply.github.com>
2022-12-09 13:18:14 +0100

    add navbar clickButton handler

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import "./App.css";
 import { Boxes } from "./components/Boxes";
 import { Footer } from "./components/Footer";
 import { Heading } from "./components/Heading";
+import { Navbar } from "./components/NavBar";
 
 function App() {
   const [date, setDate] = useState<Date>(new Date(2023, 0, 1, 0, 0));
 
+  const handleDate  = (date: Date) => {
+    setDate(date);
+  } 
+
   return (
     <div className="App">
+      <Navbar handleDate={handleDate}/>
       <div className="wrapper">
         <Heading date={date} />
         <div className="contdown">
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,16 @@
-import { useContext } from "react";
-export const Navbar = () => {
+export const Navbar = (props:{handleDate: (date: Date)=> void}) => {
+    const now = new Date();
     return (
     <div className="navbar">
       <div className="navbar-year">
-        <button className="button-change" >2023</button>
-        <button className="button-change">2024</button>
-        <button className="button-change">2025</button>
+        <button className="button-change" onClick={() => props.handleDate(new Date(now.getFullYear() + 1, 0, 1, 0, 0))}>{now.getFullYear() + 1}</button>
+        <button className="button-change" onClick={() => props.handleDate(new Date(now.getFullYear() + 2, 0, 1, 0, 0))}>{now.getFullYear() + 2}</button>
+        <button className="button-change" onClick={() => props.handleDate(new Date(now.getFullYear() + 3, 0, 1, 0, 0))}>{now.getFullYear() + 3}</button>
       </div>
       <div className="navbar-xmas">
-        <button className="button-change">XMas 2023</button>
-        <button className="button-change">XMas 2024</button>
-        <button className="button-change">XMas 2025</button>
+        <button className="button-change" onClick={() => props.handleDate(new Date(now.getFullYear() + 0, 11, 25, 0, 0))}>XMas {now.getFullYear() }</button>
+        <button className="button-change" onClick={() => props.handleDate(new Date(now.getFullYear() + 1, 11, 25, 0, 0))}>XMas {now.getFullYear() + 1}</button>
+        <button className="button-change" onClick={() => props.handleDate(new Date(now.getFullYear() + 2, 11, 25, 0, 0))}>XMas {now.getFullYear() + 2}</button>
       </div>
     </div>
   );
